72c3696cb55c70c17495da1a4f7e1265e805b1d5
Vanessa Machuca <31165097+vdmachuca@users.noreply.github.com>
2026-04-14 13:51:05 -0700

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -18,8 +18,9 @@ function Main() {
         </div>
         <div className="content">
           <h1>Vanessa Machuca</h1>
-          <p>I am a data scientist and creative thinker with a background in math, ecology, GIS, and applied remote sensing.</p>
-          <p>I want to use my skillset to support environmental justice, conservation, and climate action.</>
+          <p>I am an interdisciplinary geospatial data scientist and creative thinker with a broad background in mathematics, systems biology, ecology, geographic information systems, and remote sensing.</p>
+          <p>I am interested in supporting environmental conservation, climate resilience planning, and mending our relationships with each other and the land. 
+</>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/vdmachuca" target="_blank" rel="noreferrer"><GitHubIcon/></a>
